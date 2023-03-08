399e7146888665133afcf1e6b51d03947f8f9769
AndreaGarofalo <andreasjobs94@outlook.it>
2023-03-08 16:47:28 +0100

    added delete in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -16,4 +16,13 @@
     <div class="text-danger"><strong>Created: {{ $project->created_at }}</strong></div>
     <div class="text-danger"><strong>Updated: {{ $project->updated_at }}</strong></div>   
 </div>
+<hr>
+<div class="d-flex justify-content-start my-3">
+    <a href="{{ route('admin.projects.index') }}" class="btn btn-small btn-secondary"><i class="fa-solid fa-arrow-left me-2"></i>Go back</a>
+    <form class="mx-3" action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-small btn-danger"><i class="fa-solid fa-trash me-2"></i>Elimina</button>
+    </form>
+</div>
 @endsection
