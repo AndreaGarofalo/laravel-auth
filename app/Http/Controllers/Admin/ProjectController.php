d37ae963ba674140e75500f6db57c63f67f190db
AndreaGarofalo <andreasjobs94@outlook.it>
2023-03-08 18:06:09 +0100

    completed post creation

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+
 
 class ProjectController extends Controller
 {
@@ -23,7 +25,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.projects.create');
     }
 
     /**
@@ -31,7 +33,17 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $project = new Project();
+
+        $data['slug'] = Str::slug($data['title'], '-');
+
+        $project->fill($data);
+        
+        $project->save();
+
+        return to_route('admin.projects.show', $project->id);
     }
 
     /**
